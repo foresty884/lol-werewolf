cb1bdfd071befbe8af4a12db0254d998e94bcad3
ayumu <a.sub.yu@gmail.com>
2025-05-23 14:34:07 +0900

    1434

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // MongoDB 設定
-const uri = "mongodb://localhost:27017";
+const uri = process.env.MONGO_URI;
 const dbName = "lolTaskAssigner";
 
 let db, membersCollection, tasksCollection;
