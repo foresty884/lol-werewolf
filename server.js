80cc2b433bc0e962495ab254d74e3abd85998713
ayumu <a.sub.yu@gmail.com>
2025-05-23 13:13:35 +0900

    q

--- server.js
+++ server.js
@@ -59,6 +59,7 @@ app.post('/members', async (req, res) => {
 });
 
 // サーバー起動
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
